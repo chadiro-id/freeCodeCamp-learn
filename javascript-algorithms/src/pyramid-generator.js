a2449dbe592dbfea9b5cd450ac55b27fbd02cda0
chadiro-id <chadiro.dev@gmail.com>
2025-05-10 16:58:45 +0700

    [js-algorithms: pyramid-generator] fix reversed pyramid

--- javascript-algorithms/src/pyramid-generator.js
+++ javascript-algorithms/src/pyramid-generator.js
@@ -1,27 +1,29 @@
 function pyramid(pattern, rowNum, downwards) {
   const colNum = 1 + 2 * (rowNum - 1);
 
-  let pyramid = '\n';
+  const pyramid = [];
 
   for (let i = 0; i < rowNum; i++) {
     const colArr = new Array(colNum).fill(' ');
-    // for (let j = 0; j < i; j++) {
-    //   colArr[i] = ' ';
-    //   colArr[colArr.length - 1 - i] = ' ';
-    // }
     colArr.splice(i, colArr.length - i, ...new Array(colArr.length - i * 2).fill(pattern))
     const colString = colArr.join('') + '\n';
-    pyramid += colString;
+    pyramid.push(colString);
   }
-  if (downwards === false) {
-    const reversedPyramid = Array.from(pyramid);
-    return reversedPyramid.reverse().join('');
+
+  if (!downwards) {
+    pyramid.reverse();
   }
-  return pyramid;
+
+  return '\n' + pyramid.join('');
 }
 
 console.log(pyramid('A', 1, true));
 console.log(pyramid('B', 2, true));
 console.log(pyramid('C', 3, true));
 console.log(pyramid('D', 4, true));
+console.log(pyramid('E', 5, true));
 console.log(pyramid('E', 5, false));
+console.log(pyramid('D', 4, false));
+console.log(pyramid('C', 3, false));
+console.log(pyramid('B', 2, false));
+console.log(pyramid('A', 1, false));
