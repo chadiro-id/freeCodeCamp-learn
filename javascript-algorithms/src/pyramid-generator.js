01514c8dc9843c189d34cc28f79b16928446d1a0
chadiro-id <chadiro.dev@gmail.com>
2025-05-11 23:38:16 +0700

    [js-algorithms] update pyramid generator

--- javascript-algorithms/src/pyramid-generator.js
+++ javascript-algorithms/src/pyramid-generator.js
@@ -5,7 +5,7 @@ function pyramid(pattern, rowNum, downwards) {
 
   for (let i = 0; i < rowNum; i++) {
     const colArr = new Array(colNum).fill(' ');
-    colArr.splice(i, colArr.length - i, ...new Array(colArr.length - i * 2).fill(pattern))
+    colArr.splice(i, colArr.length - i, ...new Array(colArr.length - i * 2).fill(pattern));
     const colString = colArr.join('') + '\n';
     pyramid.push(colString);
   }
@@ -18,12 +18,12 @@ function pyramid(pattern, rowNum, downwards) {
 }
 
 console.log(pyramid('A', 1, true));
+console.log(pyramid('A', 1, false));
 console.log(pyramid('B', 2, true));
+console.log(pyramid('B', 2, false));
 console.log(pyramid('C', 3, true));
+console.log(pyramid('C', 3, false));
 console.log(pyramid('D', 4, true));
+console.log(pyramid('D', 4, false));
 console.log(pyramid('E', 5, true));
 console.log(pyramid('E', 5, false));
-console.log(pyramid('D', 4, false));
-console.log(pyramid('C', 3, false));
-console.log(pyramid('B', 2, false));
-console.log(pyramid('A', 1, false));
