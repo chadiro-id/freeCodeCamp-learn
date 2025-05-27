3031c6087335c098e9eb6eaa9cd99a8e77d3da47
chadiro-id <chadiro.dev@gmail.com>
2025-05-27 11:27:36 +0700

    Fix input validation
    Change the input validation logic to comply with the requirements

--- javascript-algorithms/complaint-form/script.js
+++ javascript-algorithms/complaint-form/script.js
@@ -1,6 +1,7 @@
 /* file: script.js */
 const fullName = document.getElementById('full-name');
 const email = document.getElementById('email');
+
 const orderNo = document.getElementById('order-no');
 const productCode = document.getElementById('product-code');
 const quantity = document.getElementById('quantity');
@@ -19,7 +20,9 @@ const otherSolution = document.getElementById('other-solution');
 
 function validateForm() {
   const validatingObject = {};
-  
+  const minComplaintDescription = otherComplaint.checked ? 20 : 0;
+  const minSolutionDescription = otherSolution.checked ? 20 : 0;
+
   validatingObject['full-name'] = fullName.value.trim() !== '';
   validatingObject['email'] = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email.value);
   validatingObject['order-no'] = /^(2024)\d{6}/.test(orderNo.value);
@@ -27,31 +30,22 @@ function validateForm() {
   validatingObject['quantity'] = quantity.value > 0;
 
   validatingObject['complaints-group'] = complaintsGroup.querySelectorAll('input[type="checkbox"]:checked').length > 0;
-  validatingObject['complaint-description'] = otherComplaint.checked && complaintDescription.value.trim().length >= 20;
+  validatingObject['complaint-description'] = complaintDescription.value.trim().length >= minComplaintDescription;
 
   validatingObject['solutions-group'] = solutionsGroup.querySelectorAll('input[type="radio"]:checked').length > 0;
-  validatingObject['solution-description'] = otherSolution.checked && solutionDescription.value.trim().length >= 20;
+  validatingObject['solution-description'] = solutionDescription.value.trim().length >= minSolutionDescription;
   
   return validatingObject;
 }
 
 function isValid(validateForm) {
-  let b = true;
   for (const key in validateForm) {
-    /*if (validateForm.hasOwnProperty(key)) {
-      if (!validateForm[key]) {
-        return false;
-      }
-    }*/
-      /*if (!validateForm[key]) {
-        return false;
-      }*/
-    b = b && validateForm.key;
+    if (!validateForm[key]) {
+      return false;
+    }
   }
-  return b;
+  return true;
 }
-
-//isValid(validateForm())
 
 form.addEventListener('change', (e) => {
   const target = e.target;
