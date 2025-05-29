afd7bffeb7d3e3cf0a738a1116498cbeefb208ba
chadiro-id <chadiro.dev@gmail.com>
2025-05-30 03:01:01 +0700

    Add new file date-conversion.js
    Creating program that using built in date function

---
+++ javascript-algorithms/__common__/date-conversion.js
@@ -0,0 +1,20 @@
+
+const currentDate = new Date();
+const currentDateFormat = `Current Date and Time: ${currentDate}`;
+console.log(currentDateFormat);
+
+function formatDateMMDDYYYY(date) {
+  const formattedDate = date.toLocaleDateString("en-US");
+  return `Formatted Date (MM/DD/YYYY): ${formattedDate}`;
+}
+console.log(formatDateMMDDYYYY(currentDate));
+
+function formatDateLong(date) {
+  const formattedDate = date.toLocaleDateString("en-US", {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  })
+  return `Formatted Date (Month Day, Year): ${formattedDate}`
+}
+console.log(formatDateLong(currentDate));
